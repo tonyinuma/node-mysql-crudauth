f6429e567f1e57d94184c6c4ffd26090afe57ed1
Tony ID <tony.inuma@gmail.com>
2022-03-28 23:35:11 -0500

    Feat: initialize express server using nodemon

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const app = express();
+
+app.set("port", process.env.PORT || 3000);
+
+app.listen(app.get("port"), () => {
+    console.log(`Server started on port ${app.get("port")}`);
+});
