748f9af8d9130aa4342d086040786b945e2fba4e
Tony ID <tony.inuma@gmail.com>
2022-03-30 23:04:25 -0500

    Add: route example and codfiguration

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ app.set("port", process.env.PORT || 3000);
 // Middlewares
 app.use(morgan("dev"));
 
+// Routes
+app.use(require("./routes"));
+
 app.listen(app.get("port"), () => {
     console.log(`Server started on port ${app.get("port")}`);
 });
