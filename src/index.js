8bf92816367447a41542661175e835bbb6c60bd0
Tony ID <tony.inuma@gmail.com>
2022-03-30 22:54:21 -0500

    Add: middleware morgan to watch requests and response data

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
 
 app.set("port", process.env.PORT || 3000);
+
+// Middlewares
+app.use(morgan("dev"));
 
 app.listen(app.get("port"), () => {
     console.log(`Server started on port ${app.get("port")}`);
