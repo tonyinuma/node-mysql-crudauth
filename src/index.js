cdcaa3ef261276d521ac268eb2e5a25f70bf1598
Tony ID <tony.inuma@gmail.com>
2022-03-30 23:27:46 -0500

    Feat: views handlebars configuration

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const morgan = require("morgan");
+const {engine} = require("express-handlebars");
+const {join} = require("path");
 
 const app = express();
 
+// Settings
 app.set("port", process.env.PORT || 3000);
+app.set("views", join(__dirname, "views"));
+app.engine(".hbs", engine({
+    defaultLayout: "main",
+    layoutsDir: join(app.get("views"), "layouts"),
+    partialsDir: join(app.get("views"), "partials"),
+    extname: ".hbs",
+    helpers: require("./lib/handlebars")
+}));
+app.set("view engine", ".hbs");
 
 // Middlewares
 app.use(morgan("dev"));
